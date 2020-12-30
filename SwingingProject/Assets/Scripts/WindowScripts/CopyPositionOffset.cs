0073586d2a01bec1be6927709076f711f5cbaa82
Grant <gdoney1701@gmail.com>
2020-12-30 16:24:53 -0500

    Added copyposition and portal scripts

---
+++ SwingingProject/Assets/Scripts/WindowScripts/CopyPositionOffset.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.HighDefinition;
+
+
+public class CopyPositionOffset : MonoBehaviour
+{
+    public Transform transformToCopy;
+    public Vector3 offset;
+
+
+    // Update is called once per frame
+    void UpdateTest()
+    {
+        transform.position = transformToCopy.position + offset;
+        transform.rotation = transformToCopy.rotation;
+    }
+
+    private void OnEnable()
+    {
+        RenderPipelineManager.beginCameraRendering += UpdateCamera;
+
+    }
+
+    private void OnDisable()
+    {
+        RenderPipelineManager.beginCameraRendering -= UpdateCamera;
+    }
+
+    void UpdateCamera(ScriptableRenderContext context,Camera camera)
+    {
+        UpdateTest();
+    }
+}
