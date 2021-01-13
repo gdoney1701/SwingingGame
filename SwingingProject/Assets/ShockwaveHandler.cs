21ef1417382a7feb140be1e9ea6999c1c5bebcd6
Grant <gdoney1701@gmail.com>
2021-01-13 12:05:21 -0500

    Added Shockwave Shader

---
+++ SwingingProject/Assets/ShockwaveHandler.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShockwaveHandler : MonoBehaviour
+{
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Renderer shadertoWork = collision.collider.GetComponent<Renderer>();
+        shadertoWork.material.SetVector("_Shockwave_Position",transform.position);
+        shadertoWork.material.SetFloat("_Shockwave_Enabled", 1);
+
+    }
+}
