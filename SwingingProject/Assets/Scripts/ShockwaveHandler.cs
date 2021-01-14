b081bb0fd440c2ac5535b955952b9931abd95f58
Grant <gdoney1701@gmail.com>
2021-01-13 20:34:43 -0500

    Modified shader speed

--- SwingingProject/Assets/Scripts/ShockwaveHandler.cs
+++ SwingingProject/Assets/Scripts/ShockwaveHandler.cs
@@ -8,22 +8,28 @@ public class ShockwaveHandler : MonoBehaviour
     public float effectMaxDistance;
     bool pulseActive = false;
     public float pulseSpeed = 2f;
+    float t = 0;
     private void OnCollisionEnter(Collision collision)
     {
+        affectedShaders.Clear();
         Renderer shadertoWork = collision.collider.GetComponent<Renderer>();
-
+        Debug.Log("bonk");
         int layerMask = 1 << 9;
         Collider[] collisionPlanes = Physics.OverlapSphere(transform.position, effectMaxDistance, layerMask);
+        Vector3 impactVel = collision.relativeVelocity;
+        float shockwaveIntensity = impactVel.magnitude/2;
         foreach(Collider groundObject in collisionPlanes)
         {
             Renderer localShader = groundObject.GetComponent<Renderer>();
             affectedShaders.Add(localShader);
+            localShader.material.SetFloat("_Shockwave_Distance", 0);
             localShader.material.SetVector("_Shockwave_Position", transform.position);
             localShader.material.SetFloat("_Shockwave_Enabled", 1);
+            localShader.material.SetFloat("_Shockwave_MaxDistance", effectMaxDistance);
+            localShader.material.SetFloat("_Shockwave_Intensity", shockwaveIntensity);
         }
         pulseActive = true;
-        InitiatePulse(shadertoWork);
-        Debug.Log(collision.relativeVelocity);
+        t = 0;
 
 
     }
@@ -36,28 +42,26 @@ public class ShockwaveHandler : MonoBehaviour
     {
         if (pulseActive)
         {
-            float movement = Time.deltaTime * pulseSpeed;
-            Debug.Log(movement);
-            if (affectedShaders.Count != 0)
+            t += Time.deltaTime;
+            float movement = pulseSpeed*Time.deltaTime * Mathf.Exp(-2*t);
+            float currentDistance = 0;
+            foreach (Renderer shader in affectedShaders)
             {
-                foreach (Renderer shader in affectedShaders)
-                {
-                    float currentDistance = shader.material.GetFloat("_Shockwave_Distance");
-                    currentDistance += movement;
-                    shader.material.SetFloat("_Shockwave_Distance", currentDistance);
+                currentDistance = shader.material.GetFloat("_Shockwave_Distance");
+                currentDistance += movement;
+                shader.material.SetFloat("_Shockwave_Distance", currentDistance);
 
-                    if (currentDistance >= shader.material.GetFloat("_Shockwave_MaxDistance"))
-                    {
-                        affectedShaders.Remove(shader);
-                    }
-                }
+
             }
-            else
+            if (currentDistance >= effectMaxDistance || Mathf.Approximately(0,movement))
             {
                 pulseActive = false;
+                affectedShaders.Clear();
+                t = 0;
             }
 
 
         }
     }
+
 }
